bd6cae56855f69d42bed600f50aaa25a1d4ebcdf
pmedim61 <pmedim61@in.ibm.com>
2021-07-09 14:22:21 +0530

    intial commit

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit, OnDestroy {
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+  }
+  ngOnDestroy() {
+  }
+
+  login() {
+    this.router.navigate(['/dashboard'])
+  }
+
+}
